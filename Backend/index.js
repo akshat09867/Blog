ea335dcc33083e6379ad845de6cae3b8d09209c7
akshat 09867 <codingmaster902@gmail.com>
2024-10-06 19:39:16 +0530

    edited

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from 'url'
 const app= express()
 const _filename=fileURLToPath(import.meta.url)
 const _dirname=path.dirname(_filename)
-app.use(express.static(path.join(_dirname,'/client/build')))
+app.use(express.static(path.join(_dirname,'/frontend/build')))
 
 app.use(bodyParser.json())
 app.use(cookieParser())
@@ -21,7 +21,7 @@ dotenv.config(
    
 app.use("/api/v1/users",userrouter)
 app.use("/api/v1/post",postrouter)
-app.get('*',(req,res)=>res.sendFile(path.join(_dirname,'/client/build/index.html')))
+app.get('*',(req,res)=>res.sendFile(path.join(_dirname,'/frontend/build/index.html')))
 Connectio()
 .then(()=>{
   app.listen(process.env.Port,()=>{
